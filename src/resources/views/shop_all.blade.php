81adb336c25798971f85bd7340ecca226139219a
NakagawaTakato <ntakato6231@gmail.com>
2024-02-14 15:07:32 +0900

    sss

--- src/resources/views/shop_all.blade.php
+++ src/resources/views/shop_all.blade.php
@@ -11,7 +11,6 @@
     <a href="#" class="container-exit"><i class="fa-solid fa-bars"></i></a>
     <p class="container-text">Rese</p>
 
-
     <div class="container-group">
         <div class="container-group-bar">
             <form class="container-group-bar" action="/search" method="get">
@@ -30,15 +29,74 @@
 </div>
 
 <div class="wrapper">
-  <div class="wrapper-img">
-    <img src="img/sushi.jpg" alt="" />
+  <div class="wrapper-box">
+    <div class="wrapper-box-img">
+      <img src="img/sushi.jpg" alt="" />
+    </div>
+    <div class="wrapper-box-content">
+      <h2 class="wrapper-box-content-ttl">
+        仙人
+      </h2>
+      <p class="wrapper-box-content-text">#東京都 #寿司</p>
+      <a href="" class="wrapper-box-content-cat">詳しくみる</a>
+
+      <i class="fa-solid fa-heart"></i>
+    </div>
   </div>
-  <div class="wrapper-content">
-    <h2 class="wrapper-content-ttl">
-      仙人
-    </h2>
-    <p class="wrapper-content-text">#東京都 #寿司</p>
-    <a href="" class="wrapper-content-cat">詳しくみる</a>
+  <!-- <div class="wrapper-box_two">
+    <div class="wrapper-box-img">
+      <img src="img/yakiniku.jpg" alt="" />
+    </div>
+    <div class="wrapper-box-content">
+      <h2 class="wrapper-box-content-ttl">
+        牛助
+      </h2>
+      <p class="wrapper-box-content-text">#大阪府 #焼肉</p>
+      <a href="" class="wrapper-box-content-cat">詳しくみる</a>
+
+      <i class="fa-solid fa-heart"></i>
+    </div>
   </div>
+  <div class="wrapper-box_three">
+    <div class="wrapper-box-img">
+      <img src="img/izakaya.jpg" alt="" />
+    </div>
+    <div class="wrapper-box-content">
+      <h2 class="wrapper-box-content-ttl">
+        戦慄
+      </h2>
+      <p class="wrapper-box-content-text">#福岡県 #居酒屋</p>
+      <a href="" class="wrapper-box-content-cat">詳しくみる</a>
+
+      <i class="fa-solid fa-heart"></i>
+    </div>
+  </div>
+  <div class="wrapper-box_four">
+    <div class="wrapper-box-img">
+      <img src="img/italian.jpg" alt="" />
+    </div>
+    <div class="wrapper-box-content">
+      <h2 class="wrapper-box-content-ttl">
+        ルーク
+      </h2>
+      <p class="wrapper-box-content-text">#東京都 #イタリアン</p>
+      <a href="" class="wrapper-box-content-cat">詳しくみる</a>
+
+      <i class="fa-solid fa-heart"></i>
+    </div>
+  </div> -->
 </div>
+
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+    // 全てのハートアイコンを選択
+    const hearts = document.querySelectorAll('.fa-heart');
+    // 各ハートアイコンにイベントリスナーを追加
+    hearts.forEach(function(heart) {
+        heart.addEventListener('click', function() {
+            this.classList.toggle('active-heart');
+        });
+    });
+});
+</script>
 @endsection
