f3f2711a1e4840f993431f062f5878c3a5feee55
NakagawaTakato <ntakato6231@gmail.com>
2024-02-18 19:16:37 +0900

    n

--- src/resources/views/my_page.blade.php
+++ src/resources/views/my_page.blade.php
@@ -10,16 +10,19 @@
     <a href="#" class="container-exit"><i class="fa-solid fa-bars"></i></a>
     <p class="container-text">Rese</p>
 
+    <p>aaaaa</p>
     
     <h1>{{ $name }}</h1>
     <img src="{{ asset($image) }}" alt="" />
     <p>{{ $city }} - {{ $shop }}</p>
 
-    <form action="/shop_all" method="post">
+
+
+    <!-- <form action="/shop_all" method="post">
         @csrf
         @method('DELETE')
         <button type="submit">Delete</button>
-    </form>
+    </form> -->
 
     <div class="container-box">
         <div class="container-box-name">
