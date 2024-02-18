1d8d8a0a5dd4032ef63e494560c732ccb06ddf80
NakagawaTakato <ntakato6231@gmail.com>
2024-02-18 13:52:11 +0900

    p

--- src/app/Http/Controllers/Shop_allController.php
+++ src/app/Http/Controllers/Shop_allController.php
@@ -8,7 +8,6 @@ class Shop_allController extends Controller
 {
     public function shop_all(Request $request)
     {
-        // GETリクエストからデータを取得
         $name = $request->query('name');
         $image = $request->query('image');
         $city = $request->query('city');
@@ -20,7 +19,6 @@ class Shop_allController extends Controller
 
     public function shop_detail(Request $request)
     {
-        // POSTリクエストからデータを取得（この例ではGETと同じビューを使用）
         $name = $request->input('name');
         $image = $request->input('image');
         $city = $request->input('city');
@@ -30,10 +28,11 @@ class Shop_allController extends Controller
         return view('shop_detail', compact('name', 'image', 'city', 'shop'));
     }
 
-    public function my_page()
+    public function my_page(Request $request)
     {
+        $name = $request->input('name');
 
         // ビューにデータを渡す
-        return view('my_page');
+        return view('my_page', compact('name'));
     }
 }
--- src/resources/views/my_page.blade.php
+++ src/resources/views/my_page.blade.php
@@ -10,9 +10,7 @@
     <a href="#" class="container-exit"><i class="fa-solid fa-bars"></i></a>
     <p class="container-text">Rese</p>
 
-    <!-- <h1>{{ $name }}</h1>
-    <img src="{{ asset($image) }}" alt="" />
-    <p>{{ $city }} - {{ $shop }}</p> -->
+    <h1>{{ $name }}</h1>
 
 
     <div class="container-box">
